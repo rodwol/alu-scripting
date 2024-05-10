a302c6a33b96e0f364ec0fd393172c444ebba04f
rodwol <r.goniche@alustudent.com>
2024-05-10 13:01:26 -0700

    commit

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 input = ARGV[0]
-matches = input.scan(/from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/)
+matches = input.scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/)
 
 # Outputs the matches
 puts matches
