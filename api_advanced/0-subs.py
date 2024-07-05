147d65f5c1fd3603d5c34e390ac2889b0846435a
rodwol <r.goniche@alustudent.com>
2024-07-05 23:22:02 +0100

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and returns
-the number of subscribers
+Module: subreddit_subscribers
+
+This module provides a function to retrieve the number of subscribers
+for a given subreddit from Reddit's API.
+
+Functions:
+    number_of_subscribers(subreddit):
+        Fetches the number of subscribers for a given subreddit.
+
+Usage:
+    To use this module, call number_of_subscribers(subreddit)
+     with the name of the subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOCS"""
+    """
+       Fetches the number of subscribers for a given subreddit.
+
+       Args:
+           subreddit (str): The name of the subreddit.
+
+       Returns:
+           int: The number of subscribers if the request is successful.
+                Returns 0 if the subreddit is not found (HTTP 404).
+
+       Raises:
+           requests.exceptions.RequestException: For other HTTP errors or issues.
+       """
     user_agent = {'User-agent': 'rodwol'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=user_agent, allow_redirects=False)
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and
-prints the titles of the first 10 hot posts listed
-for a given subreddit
+Reddit Top Ten Posts Module
+
+This module provides a function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed for a given
+subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """DOCS"""
+    """Queries the Reddit API to get the top 10 hot posts for a given
+        subreddit.
+
+        Args:
+            subreddit (str): The name of the subreddit to query.
+
+        Prints:
+            The titles of the first 10 hot posts listed in the given
+            subreddit. Prints `None` if the subreddit is invalid or an error occurs."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     user_agent = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=user_agent, allow_redirects=False)
