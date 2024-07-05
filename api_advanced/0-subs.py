bcab552ea91d24514f74a774b2650628e7803bf2
rodwol <r.goniche@alustudent.com>
2024-07-05 23:12:54 +0100

    commit

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    user_agent = {'User-agent': 'rodwol'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        subscribers = response.json()
+        return subscribers['data']['subscribers']
+    elif response.status_code == 404:
+        return 0
---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""
+a function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed
+for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        if 'children' in data:
+            for post in data['children'][:10]:
+                print(post['data']['title'])
+    elif response.status_code == 404:
+        return None
