77dd068d22eb8587d3f6d97e9a6c439109e01697
rodwol <r.goniche@alustudent.com>
2024-07-05 23:15:08 +0100

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and returns the number of subscribers
+a function that queries the Reddit API and returns
+the number of subscribers
 """
 import requests
 
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 a function that queries the Reddit API and
 prints the titles of the first 10 hot posts listed
