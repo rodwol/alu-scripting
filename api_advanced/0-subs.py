1c4b3871db9e2950dd06ef78841fe443aa855d29
rodwol <r.goniche@alustudent.com>
2024-07-06 13:32:49 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,32 +1,12 @@
 #!/usr/bin/python3
-"""
-Reddit Subscriber Checker Module
-
-This module provides functionality to check the number of
-subscribers for a given subreddit using the Reddit API.
-It includes error handling for different HTTP status
-codes and offers both unauthenticated and authenticated
-requests via the `requests` library and `praw`
-(Python Reddit API Wrapper) respectively.
-
-Functions:
-    number_of_subscribers(subreddit: str) -> Union[int, str]:
-        Returns the number of subscribers
-        Handles 200 (OK), 404 (Not Found), and 403
-        (Forbidden) status codes with appropriate responses.
-
-Example Usage:
-    - Unauthenticated: Uses `requests` to fetch subreddit details.
-    - Authenticated: Uses `praw` for a more reliable and
-    authenticated request to the Reddit API."""
+"""DOCS"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """docs"""
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-            AppleWebKit/537.36 (KHTML, like Gecko)
-            Chrome/91.0.4472.124 Safari/537.36'}
+    def number_of_subscribers(subreddit):
+    user_agent = {'User-agent': 'rodwol'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=user_agent, allow_redirects=False)
 
@@ -34,8 +14,4 @@ def number_of_subscribers(subreddit):
         subscribers = response.json()
         return subscribers['data']['subscribers']
     elif response.status_code == 404:
-        return "OK"
-    elif response.status_code == 403:
-        return "Forbidden: 403"
-    else:
-        return f"Error: {response.status_code}"
+        return 0
