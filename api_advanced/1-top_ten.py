e113317da8e3512549e60c7a202b0edad366f669
rodwol <r.goniche@alustudent.com>
2024-07-06 12:46:05 +0100

    commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()['data']
-        if 'children' in data:
-            for post in data['children'][:10]:
-                print(post['data']['title'])
-    elif response.status_code == 404:
-        return None
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
