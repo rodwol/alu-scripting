db195f832f8082f00e1fe88c3fcab40c9ded0cbe
rodwol <r.goniche@alustudent.com>
2024-07-06 12:43:14 +0100

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,16 +8,16 @@ def number_of_subscribers(subreddit):
     reddit_url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
-    header = {'User-agent': 'Mozilla/5.0'}
+    header = {'User-agent': 'rodwol'}
     response = requests.get(reddit_url,
                             headers=header
                             )
 
     if response.status_code == 200:
-        data = response.json()['data']
-        subs = data['subscribers']
-        return subs
-    return 0
+        subscribers = response.json()
+        return subscribers['data']['subscribers']
+    elif response.status_code == 404:
+        return 0
 """
 def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'rodwol'}
