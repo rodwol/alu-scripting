10490894114436e933a451e2a6f437a6b2b11d88
rodwol <r.goniche@alustudent.com>
2024-07-06 12:51:59 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,12 +4,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    user_agent = {'User-agent': 'python:com.example.myredditapp:v1.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+            AppleWebKit/537.36 (KHTML, like Gecko)
+            Chrome/91.0.4472.124 Safari/537.36'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=user_agent, allow_redirects=False)
 
     if response.status_code == 200:
         subscribers = response.json()
         return subscribers['data']['subscribers']
     elif response.status_code == 404:
-        return 0
+        return "OK"
+    elif response.status_code == 403:
+        return "Forbidden: 403"
+    else:
+        return f"Error: {response.status_code}"
