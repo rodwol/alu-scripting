ece08ad494fb32f3112fe974795d7b8d97159654
rodwol <r.goniche@alustudent.com>
2024-07-06 10:05:33 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    user_agent = {'User-agent': 'rodwol'}
+    user_agent = {'User-agent': 'python:com.example.myredditapp:v1.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=user_agent, allow_redirects=False)
 
