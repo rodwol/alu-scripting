f4067d866c1f4760363da8da3239b68fa3e74da8
rodwol <r.goniche@alustudent.com>
2024-07-06 12:44:48 +0100

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""DOC"""
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-
-    header = {'User-agent': 'rodwol'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        subscribers = response.json()
-        return subscribers['data']['subscribers']
-    elif response.status_code == 404:
+        data = response.json()["data"]
+        return data["subscribers"]
+    else:
         return 0
+
 """
 def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'rodwol'}
