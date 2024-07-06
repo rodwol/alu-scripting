d98cd8d75c3bca103d641aa34a6c932a4ac26cc4
rodwol <r.goniche@alustudent.com>
2024-07-06 13:00:47 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -27,6 +27,29 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Reddit Subscriber Checker Module
+
+This module provides a function to check the number of
+subscribers for a given subreddit using the Reddit API.
+It includes error handling for different HTTP status codes
+and demonstrates both unauthenticated and authenticated
+requests to the Reddit API using the `requests` library and
+`praw` (Python Reddit API Wrapper) respectively.
+
+Functions:
+- number_of_subscribers(subreddit):
+    Returns the number of subscribers for the specified subreddit.
+    Handles 200 (OK), 404 (Not Found), and 403 (Forbidden) status
+    odes with appropriate responses.
+    Provides an example using `requests` for unauthenticated requests.
+    Shows how to use `praw` for authenticated requests
+    to avoid rate limits and access issues.
+
+Example Usage:
+- Unauthenticated: Uses `requests` to fetch subreddit details.
+- Authenticated: Uses `praw` for a more reliable and
+authenticated request to the Reddit API."""
     user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
             AppleWebKit/537.36 (KHTML, like Gecko)
             Chrome/91.0.4472.124 Safari/537.36'}
