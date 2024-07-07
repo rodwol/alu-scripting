8ead8c83515da4c70111dfcdf1f9a2ead2d553b0
rodwol <r.goniche@alustudent.com>
2024-07-07 03:33:58 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""DOC"""
+"""This script fetches the number of subscribers of a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOC"""
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Parameters:
+    subreddit (str): The name of the subreddit.
+
+    Returns:
+    int: The number of subscribers, or 0 if the subreddit is not found.
+    """
     user_agent = {'User-agent': 'rodwol'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=user_agent, allow_redirects=False)
