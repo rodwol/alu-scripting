2691339cd10ed58882b43a4c11d3c1bc073171db
rodwol <r.goniche@alustudent.com>
2024-07-07 04:56:19 -0700

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""This script fetches the number of subscribers of a given subreddit"""
+"""DOCS"""
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-
-    Parameters:
-    subreddit (str): The name of the subreddit.
-
-    Returns:
-    int: The number of subscribers, or 0 if the subreddit is not found.
-    """
-    user_agent = {'User-agent': 'rodwol'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
+def top_ten(subreddit):
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'rodwool'}
+    response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
-        subscribers = response.json()
-        return subscribers['data']['subscribers']
-    elif response.status_code == 404:
-        return 0
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
